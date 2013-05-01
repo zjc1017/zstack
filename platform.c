1b5af72ad00cf686643280fec2985596175d42e6
Juncheng Zhang <zhangjuncheng1017@163.com>
2013-05-01 10:54:30 +0800

    修改实际4.30号
    增加了模块对上位机查询指令的应答功能，请求格式为：
    FF  00 03 02 02 01 02 对功能号01 02进行了查询
    FF  00 05   82 02 01 30  02 01

---
+++ platform.c
@@ -0,0 +1,75 @@
+#include "platform.h"
+int
+platform_is_big_endian()
+{
+	unsigned short v = 0x01;
+
+	if (v == *(unsigned char*)(&v)) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+uint32_t
+htonl(uint32_t hostlong)
+{
+	uint32_t tmpl;
+	char *p;
+	char tmpc;
+
+	if (platform_is_big_endian()) {
+		return hostlong;
+	} else {
+		tmpl = hostlong;
+		char *p = (char*)&tmpl;
+
+		// 0 <--> 3
+		tmpc = p[0];
+		p[0] = p[3];
+		p[3] = tmpc;
+
+		// 1 <-->2
+		tmpc = p[1];
+		p[1] = p[2];
+		p[2] = tmpc;
+
+		return tmpl;
+	}
+}
+
+uint16_t
+htons(uint16_t hostshort)
+{
+	uint16_t tmpl;
+	char *p;
+	char tmpc;
+
+	if (platform_is_big_endian()) {
+		return hostshort;
+	} else {
+		tmpl = hostshort;
+		char *p = (char*)&tmpl;
+
+		// 0 <--> 3
+		tmpc = p[0];
+		p[0] = p[1];
+		p[1] = tmpc;
+		return tmpl;
+	}
+}
+
+uint32_t
+ntohl(uint32_t netlong)
+{
+	return htonl(netlong);
+}
+
+uint16_t
+ntohs(uint16_t netshort)
+{
+	return htons(netshort);
+}
+
+
+
